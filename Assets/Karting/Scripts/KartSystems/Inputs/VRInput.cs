aecb0022818d1e1389d5ab6b9d83fb1afb8906c3
Ryan Wickman <rwickman@memphis.edu>
2020-02-17 19:35:33 -0600

    Began working on Google VR prototype

---
+++ Assets/Karting/Scripts/KartSystems/Inputs/VRInput.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KartGame.KartSystems
+{
+    /// <summary>
+    /// A basic keyboard implementation of the IInput interface for all the input information a kart needs.
+    /// </summary>
+    public class VRInput : MonoBehaviour, IInput
+    {
+        public float Acceleration
+        {
+            get { return m_Acceleration; }
+        }
+        public float Steering
+        {
+            get { return m_Steering; }
+        }
+        public bool BoostPressed
+        {
+            get { return m_BoostPressed; }
+        }
+        public bool FirePressed
+        {
+            get { return m_FirePressed; }
+        }
+        public bool HopPressed
+        {
+            get { return m_HopPressed; }
+        }
+        public bool HopHeld
+        {
+            get { return m_HopHeld; }
+        }
+
+        float m_Acceleration;
+        float m_Steering;
+        bool m_HopPressed;
+        bool m_HopHeld;
+        bool m_BoostPressed;
+        bool m_FirePressed;
+
+        Camera m_Camera;
+        bool m_FixedUpdateHappened;
+
+        public float minTurnThreshold = 0.05f;
+        public float steeringFactor = 1;
+        //Vector3 camLeft;
+        Vector3 camRight;
+        void Start()
+        {
+            m_Acceleration = 0.1f;
+            m_Camera = Camera.main;
+            camRight = m_Camera.transform.right;
+        }
+
+        void Update()
+        {
+            float rot_angle = Vector3.Dot(m_Camera.transform.right, -transform.up);
+            float rot_z = Mathf.Abs(rot_angle) > minTurnThreshold ? rot_angle : 0f;
+            if (rot_z > 0)
+            {   
+                m_Steering = 1f;
+            }
+            else if (rot_z < 0)
+            {
+                m_Steering = -1f;
+            }
+            else
+            {
+                m_Steering = 0f;
+            }
+            m_Steering *= steeringFactor;
+        }
+    }
+}
