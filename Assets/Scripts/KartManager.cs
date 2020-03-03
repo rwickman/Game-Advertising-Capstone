4e54ac02998592992ed3c63c12f4bc61cb92dab1
Ryan Wickman <rwickman@memphis.edu>
2020-03-03 15:47:18 -0600

    Add KartManager

---
+++ Assets/Scripts/KartManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartManager : MonoBehaviour
+{
+
+    public GameObject VRKart;
+
+    public GameObject MobileKart;
+    bool isVR;
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleIsVR()
+    {
+        isVR = !isVR;
+    }
+}
