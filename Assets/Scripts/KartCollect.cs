fc8fcdc45f03e5114f2463168cf8b195c06eac38
Ryan <rwickman@memphis.edu>
2020-03-29 19:41:15 -0500

    Add and display score

--- Assets/Scripts/KartCollect.cs
+++ Assets/Scripts/KartCollect.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class KartCollect : MonoBehaviour
@@ -9,17 +10,37 @@ public class KartCollect : MonoBehaviour
     private int coinPoints = 5;
     private int bottleCapPoints = 10;
 
+    private TextMeshProUGUI scoreText;
+    private bool scoreUpdated;
+    private void Start()
+    {
+        scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (scoreUpdated)
+        {
+            scoreText.text = "Score: " + score;
+            scoreUpdated = false;
+        }   
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Coin")
         {
+            
             score += coinPoints;
             Destroy(other.gameObject);
+            scoreUpdated = true;
         }
         if (other.tag == "Cap")
         {
+
             score += bottleCapPoints;
             Destroy(other.gameObject);
+            scoreUpdated = true;
         }
     }
 }
