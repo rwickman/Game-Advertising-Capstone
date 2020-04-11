c3f25466a011de73efd5e2111ab05e1258f9bfde
Ryan <rwickman@memphis.edu>
2020-04-11 18:02:38 -0500

    Start working on the end of the game

---
+++ Assets/ExplodeShip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyShip;
+    public GameObject explosion;
+
+    public GameObject friendLaser;
+    
+    private bool activatedExplosion;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !activatedExplosion)
+        {
+            activatedExplosion = true;
+            StartCoroutine("Explode");
+        }
+    }
+
+    IEnumerator Explode()
+    {
+        Destroy(enemyShip);
+        explosion.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Destroy(friendLaser);
+        yield return new WaitForSeconds(10f);
+        Destroy(explosion);
+    }
+
+}
---
+++ Assets/Ship.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    // Yes I know Ship and ShipMovement should be one script...
+
+    public float yOffset = 2f;
+
+    public float speed = 1f;
+    
+    private Vector3 initPos, downPos, upPos;
+    
+    private bool goingDown;
+
+    public float randomThreshold;
+    void Start()
+    {
+        initPos = transform.position;
+        downPos = initPos + Vector3.down * yOffset;
+        upPos = initPos + -Vector3.down * yOffset;
+        // Add some randomization in their floating
+        randomThreshold = Random.Range(0.5f, 1f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goingDown)
+        {
+            transform.position = Vector3.Lerp(transform.position, downPos, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, downPos) < randomThreshold)
+            {
+                goingDown = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, upPos, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, upPos) < randomThreshold)
+            {
+                goingDown = true;
+            }
+        }
+        
+    }
+}
